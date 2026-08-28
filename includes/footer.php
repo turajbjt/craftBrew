57b456b5375bb14d52d7440eefdb5e4d5da40cfd
James Turansky <turajb@gmail.com>
2026-08-28 00:51:13 -0400

    Update site footer copyright branding to BuildDesignCreate.com

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
     </div> <!-- End .container -->
     <footer>
-        <p>&copy; <?= date('Y') ?> <?= APP_NAME ?> v<?= APP_VERSION ?> &bull; Beer, Wine & Cider Craft Brewing Log & Recipe Management</p>
+        <p>&copy; <?= date('Y') ?> BuildDesignCreate.com &bull; <?= APP_NAME ?> v<?= APP_VERSION ?></p>
         <p><small>REST API endpoint available at <code>/api/v1/</code> for companion Android App</small></p>
     </footer>
 </body>
